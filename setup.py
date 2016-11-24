f17a8868190f1526e53aa24383f4d18b91b731ce
xmayeur <Pignouf2723>
2016-11-24 22:40:18 +0100

    Initial version

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name='WSIG',
-    version='',
+    name='rPIserver',
+    version='1.0',
     packages=[''],
+    data_files=[('',['rPIserver.cfg'])],
     url='',
     license='',
     author='X. Mayeur',
     author_email='xavier@mayeur.be',
-    description='',
+    description='HTTP server with custom API to control home domotic on Raspberry PI',
     requires=['bottle', 'cherrypy', 'kodipydent', 'oauther', 'configobj', 'fabric']
 )
